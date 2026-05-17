2ac50d5be1c82d30e8434e9877dbb2116f2b672a
Emily Vavrinu <evavrinu@c1r6s5.42prague.com>
2026-05-17 15:16:52 +0200

    I have renamed the function parser.c to parse_clues.c I have rewrited the code so It would be more readable

--- parse_clues.c
+++ parse_clues.c
@@ -1,35 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
+/*   parse_clues.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: evavrinu <evavrinu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/17 14:16:05 by evavrinu          #+#    #+#             */
-/*   Updated: 2026/05/17 14:16:34 by evavrinu         ###   ########.fr       */
+/*   Created: 2026/05/17 15:14:36 by evavrinu          #+#    #+#             */
+/*   Updated: 2026/05/17 15:16:20 by evavrinu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	parse_input(char *str, int *clues)
+#include <stdlib.h>
+
+int	parse_clues(char *str, int clues[16])
 {
-	int i;
+	int	i;
+
 	i = 0;
-	while (str[i])
+	while (*str && i < 16)
 	{
-		if (i % 2 == 0)
-		{
-			if (str[i] < '1' || str[i] > '4')
-				return (0);
-			clues[i / 2] = str[i] - '0';
-		}
-		else
-		{
-			if (str[i] != ' ')
-				return (0);
-		}
-		i++;
+		while (*str == ' ')
+			str++;
+		if (*str < '1' || *str > '4')
+			return (0);
+		clues[i++] = *str - '0';
+		str++;
 	}
-	if (i != 31)
-		return (0);
-	return (1);
+	return (i == 16);
 }
